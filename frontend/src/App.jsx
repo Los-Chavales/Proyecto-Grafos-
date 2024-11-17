593e5ef8fa05c531d9d79df18bc425cc2e03704c
Moisés Alfonso Terán <teranrivasma@uvm.edu.ve>
2024-11-17 02:21:55 -0400

    PDG-17 añadir residencias

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -45,12 +45,47 @@ function App() {
     }
   };
 
+  // Envía la casa seleccionada al backend para guardarlo como nodo
+  const saveHouseToBackend = async () => {
+    if (!selectedPlace) return alert('Por favor, selecciona un lugar primero.');
+
+    const response = await fetch('http://localhost:4000/houses/create_house', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        price: 300,
+        construction_materials: "acero",
+        size: 270,
+        rooms: 2,
+        property_type: `casa-${Date.now()}`,
+        house_coords: [selectedPlace.lat,selectedPlace.lng],
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjAwZTc3ZmU1LWY0MjgtNDFhNi1iNGFjLTkxMmNlYmFkNTMyZSIsIm5hbWUiOiJ1c3VhcmlvX3Byb3BpZXRhcmlvIiwicm9sIjoicHJvcGlldGFyaW8iLCJwaG9uZSI6IjA0MTQtNzc3Nzc4OCIsImlhdCI6MTczMTgyMzA0MCwiZXhwIjoxNzMxODI2NjQwfQ.W9SvLn6ZlGOshq_nr-wlEopXoqWWmyKNST46C2nqnLM",
+      }),
+    });
+
+    const info = await response.json();
+    const data = info.data;
+    console.debug(data);
+    if (response.ok) {
+      alert('Residencia guardada exitosamente');
+      setHouses((prev) => [...prev, {
+        id: data.id, name: data.property_type, lat: data.house_coords[0], lng: data.house_coords[1], type: 'house',
+      }]); // Agrega al mapa
+    } else {
+      alert('Error al guardar el lugar');
+      console.error(data.error);
+    }
+  };
+
   return (
     <>
       <div className='map'>
         <MapComponent houses={houses} places={places} onPlaceSelect={handlePlaceSelect} />
       </div>
       <button onClick={savePlaceToBackend}>Guardar Lugar</button>
+      <button onClick={saveHouseToBackend}>Guardar Casa</button>
     </>
   )
 }
