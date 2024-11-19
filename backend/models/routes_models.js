b81382b46edadd7be6aabbabbe65d0168543aac9
Paola Marcano <marcanosalaspv@uvm.edu.ve>
2024-11-19 13:28:50 -0400

    bug corregir backend

--- backend/models/routes_models.js
+++ backend/models/routes_models.js
@@ -123,6 +123,111 @@ class Router_Models {
     }  
 
   }
+
+
+  async check_relationship_byname(property_type,name_place){ //Verificar la relación por nombre
+
+    const query = `
+      MATCH
+      (h:house {property_type:$property_type})
+      -[r:route]-
+      (p:place{name_place:$name_place}) 
+      RETURN r
+    `;
+    
+    const params = {
+      property_type: property_type,
+      name_place: name_place,
+    };
+
+    let session = driver.session();
+    let resultObj;
+
+    try {
+      resultObj = await session.run(query, params);
+
+      
+      if(resultObj.records.length > 0){
+        return {
+          message: "Conexión lograda",
+          status: 200,
+          data: resultObj.records[0].get(0).properties
+        };
+      }else{
+        return {
+          message: "Conexión lograda",
+          status: 404,
+          data: "sin resultados"
+        };
+      }
+
+    }
+    catch (err) {
+      console.error(err);
+      return {
+        message: "Error de la petición",
+        status: 500,
+        data: err
+      };
+    }  
+
+  }
+
+
+  async create_route(route){ //Crear relación
+    console.log("Allá va")
+    console.log(route)
+
+    
+   let routeFound = await this.check_relationship_byname(route.house.name, route.place.place) //Verificar que no exista una relación ya registrada
+   
+    if(routeFound.status == 200){
+      return {
+        message: "Ya existe una ruta hacia allí",
+        status: 500,
+        data: route
+      };
+    } 
+
+    const query = `
+      MATCH (h:house {property_type:$property_type}),
+      (p:place {name_place:$name_place})
+
+      CREATE(h)-[r:route {distance:$distance, geometry:$geometry}]->(p)
+      RETURN r
+    `;
+    
+    const params = {
+      property_type: route.house.name,
+      name_place: route.place.place,
+      distance: route.distance,
+      geometry: route.geometry,
+    };
+
+    let session = driver.session();
+    let resultObj;
+
+    try {
+      resultObj = await session.run(query, params);
+
+      console.log("RESULT");
+      console.log(resultObj.records[0].get(0).properties);
+      return {
+        message: "Conexión lograda",
+        status: 200,
+        data: resultObj.records[0].get(0).properties
+      };
+    }
+    catch (err) {
+      console.error(err);
+      return {
+        message: "Error de la petición",
+        status: 500,
+        data: err
+      };
+    }  
+
+  }
 }
 
 module.exports = new Router_Models();
